c9b1e6c1dcd5a1d599d298f3d089de68afc2dc62
nikitavladimirov <nikvladimirov8@gmail.com>
2020-11-06 15:02:25 +0300

    Create potentiometer.cpp

---
+++ potentiometer.cpp
@@ -0,0 +1,21 @@
+// Потенциометр в связке с LCD дисплеем
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  pinMode(A2, INPUT);
+  // Print a message to the LCD.
+  lcd.print("Value:       V");
+  Serial.begin(9600);
+}
+
+void loop() {
+  int inputValue = analogRead(A2);
+  float voltage = inputValue * 5.0/1024;
+  Serial.println(voltage);
+  lcd.setCursor(8, 0);
+  lcd.print(voltage);
